2b622e9cdaf2530c3293417cd478986fcef65efc
Fenhl <fenhl@fenhl.net>
2020-12-16 14:39:05 +0000

    Add code

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use {
+    std::{
+        fs::File,
+        io,
+    },
+    byteorder::{
+        LittleEndian,
+        ReadBytesExt as _,
+    },
+    clipboard_win::{
+        Unicode,
+        set_clipboard,
+    },
+    derive_more::From,
+    error_code::SystemError,
+};
+
+#[derive(Debug, From)]
+enum Error {
+    Clipboard(SystemError),
+    HomeNotFound,
+    Io(io::Error),
+    Version,
+}
+
+fn main() -> Result<(), Error> {
+    let mut stats_file = File::open(
+        directories::UserDirs::new()
+            .ok_or(Error::HomeNotFound)?
+            .home_dir()
+            .join("AppData")
+            .join("LocalLow")
+            .join("Innersloth")
+            .join("Among Us")
+            .join("playerStats2")
+    )?;
+    let version = stats_file.read_u8()?;
+    if version != 3 { return Err(Error::Version) }
+    let mut buf = String::default();
+    loop {
+        let stat = match stats_file.read_u32::<LittleEndian>() {
+            Ok(stat) => stat,
+            Err(e) if e.kind() == io::ErrorKind::UnexpectedEof => break,
+            Err(e) => return Err(Error::Io(e)),
+        };
+        buf += &format!("{}\n", stat);
+    }
+    set_clipboard(Unicode, buf)?;
+    Ok(())
+}
